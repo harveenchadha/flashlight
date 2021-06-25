3132bc28d7b107ee4d99890a0b796d7f0b1857b4
Harveen Singh Chadha <30959215+harveenchadha@users.noreply.github.com>
2021-06-25 23:24:15 +0530

    Delete bin directory

--- bin/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/flashlight/flashlight.git
-cd flashlight/bindings/python
-export USE_MKL=0
-python setup.py install
-# python criterion_example.py  ## to test
